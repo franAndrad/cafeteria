6cf5b4485f233e8f4b0e42c1aa4cdf951588d1af
franAndrad <franciscoezequielandrade@gmail.com>
2022-08-15 23:43:17 -0300

    se agregaron las rutas y se trajo el maquetado del navbar

---
+++ src/components/views/AdministrarProductos.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdministrarProductos = () => {
+    return (
+        <div>
+            <h1>Pagina para madministrar productos</h1>
+        </div>
+    );
+};
+
+export default AdministrarProductos;
