126166b41a247e8625c05c6f4dfa74efbec4375e
franAndrad <franciscoezequielandrade@gmail.com>
2022-08-23 02:33:16 -0300

    se termino las validaciones del form

--- src/components/views/producto/CrearProducto.js
+++ src/components/views/producto/CrearProducto.js
@@ -1,34 +1,57 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { Form, Button } from 'react-bootstrap';
+import { cantidadCaracteres, validarPrecio, validarURL } from './helpers';
+
 
 const CrearProducto = () => {
+
+    // crear states
+    const [nombreProducto,setNombreProducto] = useState('');
+    const [precio,setPrecio] = useState(0);
+    const [imagen,setImagen] = useState('');
+    const [categoria,setCategoria] = useState('')
+    
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        // validar los datos
+        if(cantidadCaracteres(nombreProducto)&&validarPrecio(precio)&&validarURL(imagen)){
+            console.log('los datos son correctos crear el objeto')
+        }else{
+            console.log('solicitar que se cargen los datos correctamente')
+        }
+        // crear un objeto
+        // enviar peticion a json-server(API) create
+        
+    }
     return (
         <section className='container'>
             <h1 className='display-4 mt-5'>Pagina para crear productos</h1>
             <hr />
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formNombreProdcuto">
                     <Form.Label>Nombre producto*</Form.Label>
-                    <Form.Control type="text" placeholder="Ej: Cafe" />
+                    <Form.Control type="text" placeholder="Ej: Cafe"  onChange={(e)=>setNombreProducto(e.target.value)}/>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formPrecio">
                     <Form.Label>Precio*</Form.Label>
-                    <Form.Control type="text" placeholder="Ej: 50" />
+                    <Form.Control type="text" placeholder="Ej: 50" onChange={(e)=>setPrecio(e.target.value)}/>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formPrecio">
                     <Form.Label>Imagen URL*</Form.Label>
-                    <Form.Control type="text" placeholder="Ej: https://www.pexels.com/es-es/foto/vans-en-blanco-y-negro-fuera-de-la-decoracion-para-colgar-en-la-pared-1230679/" />
+                    <Form.Control type="text" placeholder="Ej: https://www.pexels.com/es-es/foto/vans-en-blanco-y-negro-fuera-de-la-decoracion-para-colgar-en-la-pared-1230679/" onChange={(e)=> setImagen(e.target.value)}/>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formPrecio">
                     <Form.Label>Categoria*</Form.Label>
-                    <Form.Select>
+                    <Form.Select onChange={(e)=>setCategoria(e.target.value)}>
                         <option value="">Seleccione una opcion</option>
-                        <option value="">Una</option>
-                        <option value="">Dos</option>
+                        <option value="bebida-caliente">Bebida Caliente</option>
+                        <option value="bebida-fria">Bebida Fria</option>
+                        <option value="dulce">Dulce</option>
+                        <option value="salado">Salado</option>
                     </Form.Select>
                 </Form.Group>
                 <Button variant="primary" type="submit">
-                    Submit
+                    Guardar
                 </Button>
                 </Form>
         </section>
---
+++ src/components/views/producto/helpers.js
@@ -0,0 +1,33 @@
+export const cantidadCaracteres = (input) => {
+    if(input.length>=2 && input.length<=20){
+        return true; //dato correcto
+    }else{
+        return false;
+    }
+}
+
+export const validarPrecio = (dato) =>{
+    let patron = /^[0-9]{1,4}$/;
+    if(patron.test(dato)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+export const validarURL = (input) =>{
+    let expRegular = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+    if(expRegular.test(input.value.trim())){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+export const validarCategoria = (input) =>{
+    if(input!==''){
+        return true;
+    }else{
+        return false;
+    }
+}
