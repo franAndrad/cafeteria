25756da39ac08c55ce27e795ad5d82f7ee769e72
franAndrad <franciscoezequielandrade@gmail.com>
2022-08-25 11:22:55 -0300

    se corrigio el link de la variable de entorno, se habia cambiado por error anteriormente

--- src/components/views/producto/AdministrarProductos.js
+++ src/components/views/producto/AdministrarProductos.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Table, Button } from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
 import ItemProducto from './ItemProducto';
 import { Link } from 'react-router-dom';
 
