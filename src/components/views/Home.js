32cf86891c84001e5a320e6139acf85d89016a4d
franAndrad <franciscoezequielandrade@gmail.com>
2022-08-26 00:00:13 -0300

    se termino la funcionalidad del home

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CardProducto from './producto/CardProducto';
 
 const Home = () => {
+
+    const [productos, setProductos] = useState([]);
+    const URL = process.env.REACT_APP_API_CAFETERIA;
+
+    useEffect(() => {
+        consultarAPI();
+    }, [])
+
+    const consultarAPI = async () => {
+        try {
+            const respuesta = await fetch(URL);
+            const listaProductos = await respuesta.json();
+            setProductos(listaProductos);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <section className='container mt-3'>
             <h1>Pagina Principal</h1>
             <hr />
-            <CardProducto></CardProducto>
+            <div className='row my-4'>
+            {
+                productos.map((producto, key) =>
+                <CardProducto key={key} producto={producto}></CardProducto>
+                )
+            }
+            </div>
         </section>
     );
 };
 
-export default Home;    <h1>Pagina Principal</h1>
+export default Home; <h1>Pagina Principal</h1>
--- src/components/views/producto/CardProducto.js
+++ src/components/views/producto/CardProducto.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
 
-const CardProducto = () => {
+const CardProducto = (props) => {
     return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-        </Card>
+        <div className='col-lg-3 col-md-4 col-6 mt-3 card-group'>
+            <Card>
+                <Card.Img variant="top" src={props.producto.imagen} />
+                <Card.Body>
+                    <Card.Title>{props.producto.nombreProducto}</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                    <div className='d-flex justify-content-around'>
+                        <h4>{props.producto.precio}</h4>
+                        <Button variant="primary">Ver mas</Button>
+                    </div>
+                </Card.Body>
+            </Card>
+        </div>
     );
 };
 
